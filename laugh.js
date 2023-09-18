01690c4fd63f8ab1ea750d3cab98386b8a39e699
manuelzarroca <71357308+manuelzarroca@users.noreply.github.com>
2023-09-17 20:37:34 -0600

    Update laugh.js

--- laugh.js
+++ laugh.js
@@ -193,4 +193,5 @@ var puns = function () {
   var newpun = punsarray[Math.floor(Math.random() * punsarray.length)];
   document.getElementById("laugh-text").innerHTML = newpun;
   document.getElementById("sharepun").value = newpun;
+  document.getElementById("shareprev").innerHTML = newpun;
 };
